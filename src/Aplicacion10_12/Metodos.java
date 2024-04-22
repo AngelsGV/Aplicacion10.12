3afc8404ad953dbc232ac7126cbfa506a059da86
AngelsGV <angels.not99@gmail.com>
2024-04-22 14:37:58 +0200

    Initial commit

--- src/Aplicacion10_12/Metodos.java
+++ src/Aplicacion10_12/Metodos.java
@@ -2,31 +2,6 @@ package Aplicacion10_12;
 import java.io.*; //Declaració  de flujos de entrada de texto
 
 public class Metodos {
-    //public static void mostrarDatos(){
-    //String texto = ""; //Usem una cadena buida de moment
-    //BufferedReader in = null;
-
-   //try {
-     //  in = new BufferedReader(new FileReader("texto.txt"));
-       //String linea = in.readLine();
-       //while (linea != null) { //mentre no arribe al final del archiu
-         //  texto = texto + linea + '\n'; //out.newLine(); // canvi de línea en el archiu
-          // linea = in.readLine();
-       //}
-   //}catch(Exception ex){
-     //       System.out.println(ex.getMessage());
-   //} finally{
-     //  if (in != null){
-       //    try {
-         //      in.close();//Buidem el búfer i se escriu el archiu
-          // }catch (Exception ex){
-            //   System.out.println(ex);
-           //}
-       //}
-    //}}
-      // System.out.println(texto);
-//Este fue el primer metodo que cree, me pensaba que también podría pedir los datos e introducirlos aquí. Pero no.
-    //He de usar un BufferWeiter porque en este caso no solo queremos leer el archivo, sino que queremos escribir en él
 public static void guardarDatos(String nombre, int edad){
     //Hubo un momento que pensé en poner un String para que me devolviera un resultado que luego pudiera sacar por pantalla.
     //Pero al ser un método es mejor usar un void para que después de cumplir su mision, que es escribir los datos nuevos
@@ -38,7 +13,7 @@ public static void guardarDatos(String nombre, int edad){
             out.write(nombre + "," + edad); //escribimos en una unica sentencia
             out.newLine();//cambio de linea del archivo
             System.out.println("Datos guardados correctamente.");//Si llega hasta el final nos mostrará este mensaje
-            System.out.println("Nombre: " + nombre + " ,Edad: " +  edad);
+            //He mirado en el archivo datos.txt y no se sobreescribe!
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
 
@@ -54,8 +29,5 @@ public static void guardarDatos(String nombre, int edad){
 }
 }
 //Problemas
-// En consola no me guarda como toca -->
-//Datos guardados correctamente.
-//texto.txt (El sistema no puede encontrar el archivo especificado) -->el metodo mostrarDatos no es necesario en este caso.
-//Solo nos pide introducir los datos y que si el archivo está que lo copie en otra línea
-//Lo que hace mi programa es sobreescribir los datos y no deja los anteriores.
+
+//Lo que hace mi programa es sobreescribir los datos y no deja los anteriores. --> Solución appennd true en el conductor
